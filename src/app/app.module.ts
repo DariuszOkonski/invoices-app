b5b8823b7d813129dd6df822e1d28f8f96d92a41
Dariusz Okonski <darek200180@gmail.com>
2022-02-06 03:46:13 +0000

    add invoice-preview

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,15 @@ import { AppComponent } from './app.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
 import { InvoicesListComponent } from './components/invoices-list/invoices-list.component';
 import { InvoiceNewComponent } from './components/invoice-new/invoice-new.component';
+import { InvoicePreviewComponent } from './components/invoice-preview/invoice-preview.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavbarComponent,
     InvoicesListComponent,
-    InvoiceNewComponent
+    InvoiceNewComponent,
+    InvoicePreviewComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/components/invoice-preview/invoice-preview.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-invoice-preview',
+  templateUrl: './invoice-preview.component.html',
+  styleUrls: ['./invoice-preview.component.css']
+})
+export class InvoicePreviewComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
