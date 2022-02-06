391a1ff3b752c9f34c7656915ea49f6656e01a57
Dariusz Okonski <darek200180@gmail.com>
2022-02-06 19:28:56 +0000

    fix bug with wrong date in invoices at data.service

--- src/app/services/data.service.ts
+++ src/app/services/data.service.ts
@@ -11,6 +11,72 @@ const fakeSuppliers: Supplier[] = [
 
 // const fakeSuppliers: Supplier[] = [];
 
+
+// const fakeInvoices: Invoice[] = [
+//   {
+//     number: 'dc0ff5f1-0126-40b1-bbd7-afff24d2252b',
+//     orderAt: new Date('02/02/2022 18:54:23'),
+//     amount: 11289.00,
+//     supplier: {id: 'fd05b131-9ceb-4dc3-97f8-ed69b719a1c3', name: 'Infinite Sp. z o.o.', address: 'ul.Projektowa 1, 20-209 Lublin'},
+//   },
+//   {
+//     number: '157a70cf-1c69-4341-9441-202c7858e740',
+//     orderAt: new Date('22/12/2021 15:43:20'),
+//     amount: 32987.90,
+//     supplier: {id: 'f3469a56-29c1-4912-aa30-c1dac1e37e01', name: 'Moovem Sp z o.o', address: 'Al. Jerozolimskie 100. 00-807 Warszawa '},
+//   },
+//   {
+//     number: '91e2fa9b-0d1e-4e5d-b58a-70d37e5220f7',
+//     orderAt: new Date('01/01/2022 10:54:23'),
+//     amount: 123654.23,
+//     supplier: {id: 'fd05b131-9ceb-4dc3-97f8-ed69b719a1c3', name: 'Infinite Sp. z o.o.', address: 'ul.Projektowa 1, 20-209 Lublin'},
+//   },
+//   {
+//     number: '38f07dea-29e7-4a7c-b147-7d18de8e8533',
+//     orderAt: new Date('05/01/2022 14:23:32'),
+//     amount: 38980.00,
+//     supplier: {id: 'bef10e62-e49a-4437-9766-9fe5cf553eb4', name: 'Divante', address: 'ul.Dmowskiego 17, 50-203 Wrocław'},
+//   },
+//   {
+//     number: '5290183c-f700-45e0-a185-a69f6db7cb33',
+//     orderAt: new Date('31/12/2021 12:21:42'),
+//     amount: 456789.09,
+//     supplier: {id: 'f3469a56-29c1-4912-aa30-c1dac1e37e01', name: 'Moovem Sp z o.o', address: 'Al. Jerozolimskie 100. 00-807 Warszawa '},
+//   },
+//   {
+//     number: 'c641432e-c170-4f87-b253-a662e0d44f49',
+//     orderAt: new Date('18/11/2021 21:12:43'),
+//     amount: 236789.98,
+//     supplier: {id: 'fd05b131-9ceb-4dc3-97f8-ed69b719a1c3', name: 'Infinite Sp. z o.o.', address: 'ul.Projektowa 1, 20-209 Lublin'},
+//   },
+//   {
+//     number: '04f03323-2014-4f56-9dbf-fcccfa3fd9b0',
+//     orderAt: new Date('10/10/2021 14:21:42'),
+//     amount: 654123.99,
+//     supplier: {id: 'f3469a56-29c1-4912-aa30-c1dac1e37e01', name: 'Moovem Sp z o.o', address: 'Al. Jerozolimskie 100. 00-807 Warszawa '},
+//   },
+//   {
+//     number: '3e03b149-f234-42c0-946f-b0033dd354bd',
+//     orderAt: new Date('11/10/2021 11:23:21'),
+//     amount: 123546.00,
+//     supplier: {id: 'bef10e62-e49a-4437-9766-9fe5cf553eb4', name: 'Divante', address: 'ul.Dmowskiego 17, 50-203 Wrocław'},
+//   },
+//   {
+//     number: 'adf3d87b-ea96-4657-b227-20df1fffb326',
+//     orderAt: new Date('15/12/2021 12:24:32'),
+//     amount: 121901.00,
+//     supplier: {id: 'fd05b131-9ceb-4dc3-97f8-ed69b719a1c3', name: 'Infinite Sp. z o.o.', address: 'ul.Projektowa 1, 20-209 Lublin'},
+//   },
+//   {
+//     number: 'adf3d87b-ea96-4657-b227-20df1fffb324',
+//     orderAt: new Date('18/12/2021 12:43:21'),
+//     amount: 321980.21,
+//     supplier: {id: 'fd05b131-9ceb-4dc3-97f8-ed69b719a1c3', name: 'Infinite Sp. z o.o.', address: 'ul.Projektowa 1, 20-209 Lublin'},
+//   },
+
+// ]
+// const fakeInvoices: Invoice[] =[];
+
 const fakeInvoices: Invoice[] = [
   {
     number: 'dc0ff5f1-0126-40b1-bbd7-afff24d2252b',
@@ -20,7 +86,7 @@ const fakeInvoices: Invoice[] = [
   },
   {
     number: '157a70cf-1c69-4341-9441-202c7858e740',
-    orderAt: new Date('22/12/2021 15:43:20'),
+    orderAt: new Date('03/02/2022 18:54:23'),
     amount: 32987.90,
     supplier: {id: 'f3469a56-29c1-4912-aa30-c1dac1e37e01', name: 'Moovem Sp z o.o', address: 'Al. Jerozolimskie 100. 00-807 Warszawa '},
   },
@@ -38,13 +104,13 @@ const fakeInvoices: Invoice[] = [
   },
   {
     number: '5290183c-f700-45e0-a185-a69f6db7cb33',
-    orderAt: new Date('31/12/2021 12:21:42'),
+    orderAt: new Date('05/01/2022 14:23:32'),
     amount: 456789.09,
     supplier: {id: 'f3469a56-29c1-4912-aa30-c1dac1e37e01', name: 'Moovem Sp z o.o', address: 'Al. Jerozolimskie 100. 00-807 Warszawa '},
   },
   {
     number: 'c641432e-c170-4f87-b253-a662e0d44f49',
-    orderAt: new Date('18/11/2021 21:12:43'),
+    orderAt: new Date('01/01/2022 10:54:23'),
     amount: 236789.98,
     supplier: {id: 'fd05b131-9ceb-4dc3-97f8-ed69b719a1c3', name: 'Infinite Sp. z o.o.', address: 'ul.Projektowa 1, 20-209 Lublin'},
   },
@@ -61,20 +127,22 @@ const fakeInvoices: Invoice[] = [
     supplier: {id: 'bef10e62-e49a-4437-9766-9fe5cf553eb4', name: 'Divante', address: 'ul.Dmowskiego 17, 50-203 Wrocław'},
   },
   {
-    number: 'adf3d87b-ea96-4657-b227-20df1fffb324',
-    orderAt: new Date('15/12/2021 12:24:32'),
+    number: 'adf3d87b-ea96-4657-b227-20df1fffb326',
+    orderAt: new Date('11/10/2021 11:23:21'),
     amount: 121901.00,
     supplier: {id: 'fd05b131-9ceb-4dc3-97f8-ed69b719a1c3', name: 'Infinite Sp. z o.o.', address: 'ul.Projektowa 1, 20-209 Lublin'},
   },
   {
     number: 'adf3d87b-ea96-4657-b227-20df1fffb324',
-    orderAt: new Date('18/12/2021 12:43:21'),
+    orderAt: new Date('01/01/2022 10:54:23'),
     amount: 321980.21,
     supplier: {id: 'fd05b131-9ceb-4dc3-97f8-ed69b719a1c3', name: 'Infinite Sp. z o.o.', address: 'ul.Projektowa 1, 20-209 Lublin'},
   },
 
 ]
-// const fakeInvoices: Invoice[] =[];
+
+
+
 @Injectable({
   providedIn: 'root'
 })
@@ -106,17 +174,13 @@ export class DataService {
 
   // TODO - temporarly, write logic
   getInvoice(id: string) {
-    const invoice: Invoice = {
-      number: '1',
-      orderAt: new Date(),
-      amount: 11546.43,
-      supplier: {
-        id: '2',
-        name: 'Microsoft Software',
-        address: '50 main str, London'
-      }
-    }
-    return invoice;
+    let tempInvoice: any;
+    tempInvoice = this.invoices.find(inv => inv.number === id);
+
+    console.log(tempInvoice);
+
+
+    return this.invoices.filter(invoice => invoice.number === id);
   }
 
   // TODO - temporarly, write logic
