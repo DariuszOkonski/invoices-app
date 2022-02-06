84dad32cdf36bdcfc4a8b70d25b0bb0a97027c07
Dariusz Okonski <darek200180@gmail.com>
2022-02-06 21:02:20 +0000

    finish task to add new invoice

--- src/app/components/invoice-new/invoice-new.component.ts
+++ src/app/components/invoice-new/invoice-new.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { DataService } from 'src/app/services/data.service';
+import { Supplier } from './../../models/Supplier';
+import { Invoice } from './../../models/Invoice';
+
 
 @Component({
   selector: 'app-invoice-new',
@@ -6,10 +10,55 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./invoice-new.component.css']
 })
 export class InvoiceNewComponent implements OnInit {
+  number: string;
+  date: Date;
+  amount: number;
+  supplier: Supplier;
+  suppliers: Supplier[]
 
-  constructor() { }
+  supplierSelected: string = '';
+
+  constructor(
+    private dataService: DataService
+  ) {
+    this.number = this.generateId();
+    this.date = new Date();
+    this.amount = 0;
+    this.supplier = {
+      id: '',
+      name: '',
+      address: ''
+    }
+
+    this.suppliers = [...this.dataService.getSuppliers()];
+    if(this.suppliers.length > 0) {
+      if(this.suppliers[0].id === 'all')
+        this.suppliers.splice(0, 1);      
+    }
+  }
 
   ngOnInit(): void {
   }
 
+  updateSelectedSupplier(event: Event) {
+    this.supplier = this.dataService.getSupplier(this.supplierSelected);
+  }
+
+  onAddNewInvoice() {
+    const newInvoice: Invoice = {
+      number: this.number,
+      orderAt: this.date,
+      supplier: this.supplier,
+      amount: this.amount
+    }
+
+    this.dataService.addNewInvoice(newInvoice);   
+  }
+
+  generateId() {
+    return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
+        var r = Math.random()*16|0, v = c == 'x' ? r : (r&0x3|0x8);
+        return v.toString(16);
+    });
+  }
 }
