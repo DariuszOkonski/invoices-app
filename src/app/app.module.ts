8e2154247e989113feec304297573824d6272413
Dariusz Okonski <darek200180@gmail.com>
2022-02-06 04:08:51 +0000

    improve views

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,6 +8,7 @@ import { InvoicesListComponent } from './components/invoices-list/invoices-list.
 import { InvoiceNewComponent } from './components/invoice-new/invoice-new.component';
 import { InvoicePreviewComponent } from './components/invoice-preview/invoice-preview.component';
 import { SupplierPreviewComponent } from './components/supplier-preview/supplier-preview.component';
+import { NotFoundComponent } from './components/not-found/not-found.component';
 
 @NgModule({
   declarations: [
@@ -16,7 +17,8 @@ import { SupplierPreviewComponent } from './components/supplier-preview/supplier
     InvoicesListComponent,
     InvoiceNewComponent,
     InvoicePreviewComponent,
-    SupplierPreviewComponent
+    SupplierPreviewComponent,
+    NotFoundComponent
   ],
   imports: [
     BrowserModule,
