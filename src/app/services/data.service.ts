86f4a99fa08e8a6e7119a2a9fd8666a7e69bb808
Dariusz Okonski <darek200180@gmail.com>
2022-02-07 01:56:17 +0000

    refactor data.service.ts

--- src/app/services/data.service.ts
+++ src/app/services/data.service.ts
@@ -1,80 +1,8 @@
 import { Injectable } from '@angular/core';
 import { Roles } from '../models/Roles';
+import { fakeInvoices, fakeSuppliers, INVOICES, SUPPLIERS, ALL } from '../utilities/utilities';
 import { Invoice } from './../models/Invoice';
 import { Supplier } from './../models/Supplier';
-
-const fakeSuppliers: Supplier[] = [
-  {id: 'all', name: 'All', address: ''},
-  {id: 'fd05b131-9ceb-4dc3-97f8-ed69b719a1c3', name: 'Infinite Sp. z o.o.', address: 'ul.Projektowa 1, 20-209 Lublin'},
-  {id: 'f3469a56-29c1-4912-aa30-c1dac1e37e01', name: 'Moovem Sp z o.o', address: 'Al. Jerozolimskie 100. 00-807 Warszawa '},
-  {id: 'bef10e62-e49a-4437-9766-9fe5cf553eb4', name: 'Divante', address: 'ul.Dmowskiego 17, 50-203 Wrocław'},
-  {id: 'bef10e62-e26a-4437-9766-9fe5cf553ec4', name: 'Comarch', address: 'ul.Kazimierz 17, 51-288 Kraków'},
-]
-
-const fakeInvoices: Invoice[] = [
-  {
-    number: 'dc0ff5f1-0126-40b1-bbd7-afff24d2252b',
-    orderAt: new Date('02/02/2022 18:54:23'),
-    amount: 11289.00,
-    supplier: {id: 'fd05b131-9ceb-4dc3-97f8-ed69b719a1c3', name: 'Infinite Sp. z o.o.', address: 'ul.Projektowa 1, 20-209 Lublin'},
-  },
-  {
-    number: '157a70cf-1c69-4341-9441-202c7858e740',
-    orderAt: new Date('03/02/2022 18:54:23'),
-    amount: 32987.90,
-    supplier: {id: 'f3469a56-29c1-4912-aa30-c1dac1e37e01', name: 'Moovem Sp z o.o', address: 'Al. Jerozolimskie 100. 00-807 Warszawa '},
-  },
-  {
-    number: '91e2fa9b-0d1e-4e5d-b58a-70d37e5220f7',
-    orderAt: new Date('01/01/2022 10:54:23'),
-    amount: 123654.23,
-    supplier: {id: 'fd05b131-9ceb-4dc3-97f8-ed69b719a1c3', name: 'Infinite Sp. z o.o.', address: 'ul.Projektowa 1, 20-209 Lublin'},
-  },
-  {
-    number: '38f07dea-29e7-4a7c-b147-7d18de8e8533',
-    orderAt: new Date('05/01/2022 14:23:32'),
-    amount: 38980.00,
-    supplier: {id: 'bef10e62-e49a-4437-9766-9fe5cf553eb4', name: 'Divante', address: 'ul.Dmowskiego 17, 50-203 Wrocław'},
-  },
-  {
-    number: '5290183c-f700-45e0-a185-a69f6db7cb33',
-    orderAt: new Date('05/01/2022 14:23:32'),
-    amount: 456789.09,
-    supplier: {id: 'f3469a56-29c1-4912-aa30-c1dac1e37e01', name: 'Moovem Sp z o.o', address: 'Al. Jerozolimskie 100. 00-807 Warszawa '},
-  },
-  {
-    number: 'c641432e-c170-4f87-b253-a662e0d44f49',
-    orderAt: new Date('01/01/2022 10:54:23'),
-    amount: 236789.98,
-    supplier: {id: 'fd05b131-9ceb-4dc3-97f8-ed69b719a1c3', name: 'Infinite Sp. z o.o.', address: 'ul.Projektowa 1, 20-209 Lublin'},
-  },
-  {
-    number: '04f03323-2014-4f56-9dbf-fcccfa3fd9b0',
-    orderAt: new Date('10/10/2021 14:21:42'),
-    amount: 654123.99,
-    supplier: {id: 'f3469a56-29c1-4912-aa30-c1dac1e37e01', name: 'Moovem Sp z o.o', address: 'Al. Jerozolimskie 100. 00-807 Warszawa '},
-  },
-  {
-    number: '3e03b149-f234-42c0-946f-b0033dd354bd',
-    orderAt: new Date('11/10/2021 11:23:21'),
-    amount: 123546.00,
-    supplier: {id: 'bef10e62-e49a-4437-9766-9fe5cf553eb4', name: 'Divante', address: 'ul.Dmowskiego 17, 50-203 Wrocław'},
-  },
-  {
-    number: 'adf3d87b-ea96-4657-b227-20df1fffb326',
-    orderAt: new Date('11/10/2021 11:23:21'),
-    amount: 121901.00,
-    supplier: {id: 'fd05b131-9ceb-4dc3-97f8-ed69b719a1c3', name: 'Infinite Sp. z o.o.', address: 'ul.Projektowa 1, 20-209 Lublin'},
-  },
-  {
-    number: 'adf3d87b-ea96-4657-b227-20df1fffb324',
-    orderAt: new Date('01/01/2022 10:54:23'),
-    amount: 321980.21,
-    supplier: {id: 'fd05b131-9ceb-4dc3-97f8-ed69b719a1c3', name: 'Infinite Sp. z o.o.', address: 'ul.Projektowa 1, 20-209 Lublin'},
-  },
-
-]
-
 
 @Injectable({
   providedIn: 'root'
@@ -97,12 +25,12 @@ export class DataService {
   }
 
   getSuppliersFromLocalStorage() {
-    if(localStorage.getItem('suppliers') === null) {
+    if(localStorage.getItem(SUPPLIERS) === null) {
       this.suppliers = [...fakeSuppliers];
-      localStorage.setItem('suppliers', JSON.stringify(this.suppliers));
+      localStorage.setItem(SUPPLIERS, JSON.stringify(this.suppliers));
       this.getSuppliersFromLocalStorage();
     } else {
-      const tempSuppliers = localStorage.getItem('suppliers')
+      const tempSuppliers = localStorage.getItem(SUPPLIERS)
       if(tempSuppliers !== null) {
         this.suppliers = JSON.parse(tempSuppliers);
       }
@@ -110,12 +38,12 @@ export class DataService {
   }
 
   getInvoicesFromLocalStorage() {
-    if(localStorage.getItem('invoices') === null) {
+    if(localStorage.getItem(INVOICES) === null) {
       this.invoices = [...fakeInvoices];
-      localStorage.setItem('invoices', JSON.stringify(this.invoices));
+      localStorage.setItem(INVOICES, JSON.stringify(this.invoices));
       this.getInvoicesFromLocalStorage();
     } else {
-      const tempInvoices = localStorage.getItem('invoices');
+      const tempInvoices = localStorage.getItem(INVOICES);
       if(tempInvoices !== null) {
         this.invoices = JSON.parse(tempInvoices);
       }
@@ -159,7 +87,7 @@ export class DataService {
   }
 
   filterInvoicesBySupplierName(id: string) {
-    if(id === 'all') {
+    if(id === ALL) {
       return this.invoices;
     }
 
@@ -168,7 +96,7 @@ export class DataService {
 
   addNewInvoice(invoice: Invoice) {
     this.invoices.push(invoice);
-    localStorage.setItem('invoices', JSON.stringify(this.invoices));
+    localStorage.setItem(INVOICES, JSON.stringify(this.invoices));
   }
 
   getInvoice(id: string) {
@@ -201,8 +129,8 @@ export class DataService {
       return inv
     })
 
-    localStorage.setItem('suppliers', JSON.stringify(this.suppliers));
-    localStorage.setItem('invoices', JSON.stringify(this.invoices));
+    localStorage.setItem(SUPPLIERS, JSON.stringify(this.suppliers));
+    localStorage.setItem(INVOICES, JSON.stringify(this.invoices));
   }
 
   getSupplier(id: string) {
--- src/app/utilities/utilities.ts
+++ src/app/utilities/utilities.ts
@@ -1,4 +1,6 @@
 import { Roles } from 'src/app/models/Roles';
+import { Supplier } from './../models/Supplier';
+import { Invoice } from './../models/Invoice';
 
 export function isPermitedHighAccess(currentRole: string) {
     if(currentRole === Roles.ADMIN || currentRole === Roles.USER)
@@ -13,3 +15,79 @@ export function isPermitedLowAccess(currentRole: string) {
     
     return false;
   }
+
+export const SUPPLIERS = 'suppliers'
+export const INVOICES = 'invoices'
+export const ALL = 'all';
+
+export const fakeSuppliers: Supplier[] = [
+  {id: 'all', name: 'All', address: ''},
+  {id: 'fd05b131-9ceb-4dc3-97f8-ed69b719a1c3', name: 'Infinite Sp. z o.o.', address: 'ul.Projektowa 1, 20-209 Lublin'},
+  {id: 'f3469a56-29c1-4912-aa30-c1dac1e37e01', name: 'Moovem Sp z o.o', address: 'Al. Jerozolimskie 100. 00-807 Warszawa '},
+  {id: 'bef10e62-e49a-4437-9766-9fe5cf553eb4', name: 'Divante', address: 'ul.Dmowskiego 17, 50-203 Wrocław'},
+  {id: 'bef10e62-e26a-4437-9766-9fe5cf553ec4', name: 'Comarch', address: 'ul.Kazimierz 17, 51-288 Kraków'},
+]
+
+export const fakeInvoices: Invoice[] = [
+  {
+    number: 'dc0ff5f1-0126-40b1-bbd7-afff24d2252b',
+    orderAt: new Date('02/02/2022 18:54:23'),
+    amount: 11289.00,
+    supplier: {id: 'fd05b131-9ceb-4dc3-97f8-ed69b719a1c3', name: 'Infinite Sp. z o.o.', address: 'ul.Projektowa 1, 20-209 Lublin'},
+  },
+  {
+    number: '157a70cf-1c69-4341-9441-202c7858e740',
+    orderAt: new Date('03/02/2022 18:54:23'),
+    amount: 32987.90,
+    supplier: {id: 'f3469a56-29c1-4912-aa30-c1dac1e37e01', name: 'Moovem Sp z o.o', address: 'Al. Jerozolimskie 100. 00-807 Warszawa '},
+  },
+  {
+    number: '91e2fa9b-0d1e-4e5d-b58a-70d37e5220f7',
+    orderAt: new Date('01/01/2022 10:54:23'),
+    amount: 123654.23,
+    supplier: {id: 'fd05b131-9ceb-4dc3-97f8-ed69b719a1c3', name: 'Infinite Sp. z o.o.', address: 'ul.Projektowa 1, 20-209 Lublin'},
+  },
+  {
+    number: '38f07dea-29e7-4a7c-b147-7d18de8e8533',
+    orderAt: new Date('05/01/2022 14:23:32'),
+    amount: 38980.00,
+    supplier: {id: 'bef10e62-e49a-4437-9766-9fe5cf553eb4', name: 'Divante', address: 'ul.Dmowskiego 17, 50-203 Wrocław'},
+  },
+  {
+    number: '5290183c-f700-45e0-a185-a69f6db7cb33',
+    orderAt: new Date('05/01/2022 14:23:32'),
+    amount: 456789.09,
+    supplier: {id: 'f3469a56-29c1-4912-aa30-c1dac1e37e01', name: 'Moovem Sp z o.o', address: 'Al. Jerozolimskie 100. 00-807 Warszawa '},
+  },
+  {
+    number: 'c641432e-c170-4f87-b253-a662e0d44f49',
+    orderAt: new Date('01/01/2022 10:54:23'),
+    amount: 236789.98,
+    supplier: {id: 'fd05b131-9ceb-4dc3-97f8-ed69b719a1c3', name: 'Infinite Sp. z o.o.', address: 'ul.Projektowa 1, 20-209 Lublin'},
+  },
+  {
+    number: '04f03323-2014-4f56-9dbf-fcccfa3fd9b0',
+    orderAt: new Date('10/10/2021 14:21:42'),
+    amount: 654123.99,
+    supplier: {id: 'f3469a56-29c1-4912-aa30-c1dac1e37e01', name: 'Moovem Sp z o.o', address: 'Al. Jerozolimskie 100. 00-807 Warszawa '},
+  },
+  {
+    number: '3e03b149-f234-42c0-946f-b0033dd354bd',
+    orderAt: new Date('11/10/2021 11:23:21'),
+    amount: 123546.00,
+    supplier: {id: 'bef10e62-e49a-4437-9766-9fe5cf553eb4', name: 'Divante', address: 'ul.Dmowskiego 17, 50-203 Wrocław'},
+  },
+  {
+    number: 'adf3d87b-ea96-4657-b227-20df1fffb326',
+    orderAt: new Date('11/10/2021 11:23:21'),
+    amount: 121901.00,
+    supplier: {id: 'fd05b131-9ceb-4dc3-97f8-ed69b719a1c3', name: 'Infinite Sp. z o.o.', address: 'ul.Projektowa 1, 20-209 Lublin'},
+  },
+  {
+    number: 'adf3d87b-ea96-4657-b227-20df1fffb324',
+    orderAt: new Date('01/01/2022 10:54:23'),
+    amount: 321980.21,
+    supplier: {id: 'fd05b131-9ceb-4dc3-97f8-ed69b719a1c3', name: 'Infinite Sp. z o.o.', address: 'ul.Projektowa 1, 20-209 Lublin'},
+  },
+
+]
